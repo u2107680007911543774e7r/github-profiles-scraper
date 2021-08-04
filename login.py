b35c7a5b3f854abd8fe73ee8f5a51174577762ba
Vadim Chumasov <vadim.chumasov@gmail.com>
2021-08-04 22:30:53 +0300

    Update login.py

--- login.py
+++ login.py
@@ -6,8 +6,8 @@ from bs4 import BeautifulSoup
 def main():
     try:
         session = requests.session()
-        data = {'login':'7zwei',
-                'password': '1dreamer!!'}
+        data = {'login':'your-login',
+                'password': ********'}
         session.post('https://github.com/login',data=data)
         r = session.get('https://github.com/search?l=Python&p=1&q=location%3A%22Hong+Kong%22+repos%3A%3E4+language%3APython&ref=advsearch&type=Users&utf8=%E2%9C%93')
         soup = BeautifulSoup(r.text, 'html5lib')
